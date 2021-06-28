1f026c776c7c5d3229ccb97b54cf8c685a4ea8c9
doudou5628891 <2783800599@qq.com>
2021-06-28 22:26:00 +0800

    threeSum complete

--- src/Hash/threeSum.java
+++ src/Hash/threeSum.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 
 public class threeSum {
-    public static List<List<Integer>> threeSum(int[] nums) {
+    public static List<List<Integer>> threeSum_abortion(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
         for (int i = 0; i < nums.length; i++) {
             HashMap<Integer, Integer> map = new HashMap<>();
@@ -15,6 +15,8 @@ public class threeSum {
                     temp.add(nums[i]);
                     temp.add(nums[j]);
                     temp.add(0 - nums[i] - nums[j]);
+                    Collections.sort(temp);
+
                     for (int t : temp) {
                         if (test.containsKey(t)) {
                             test.put(t, test.get(t) + 1);
@@ -22,7 +24,20 @@ public class threeSum {
                             test.put(t, 1);
                         }
                     }
-
+                    HashMap<Integer, Integer> inClone = (HashMap<Integer, Integer>) test.clone();
+                    int inCount=0;
+                    boolean inJudge = false;
+                    for(int f:nums){
+                        if(inClone.containsKey(f)&&inClone.get(f) > 0){
+                            inClone.put(f,inClone.get(f)-1);
+                            inCount++;
+                        }
+                        if (inCount == 3) {
+                            inJudge = true;
+                            break;
+                        }
+                    }
+                    if(!inJudge)continue;
                     boolean judge = false;
                     for (List<Integer> k : result) {
                         int count = 0;
@@ -48,6 +63,32 @@ public class threeSum {
             }
         }
         return result;
+    }//超出时间限制。。。。。
+    public static List<List<Integer>> threeSum(int[] nums) {
+        int n = nums.length;
+        Arrays.sort(nums);
+        List<List<Integer>> ans = new ArrayList<List<Integer>>();
+        // 枚举a
+        for (int first = 0;first<n;++first){
+            if(first>0&&nums[first]==nums[first-1])continue;
+            int third = n-1;
+            int target = -nums[first];
+            //枚举b
+            for (int second = first+1; second < n; ++second) {
+                if (second>first+1&&nums[second]==nums[second-1])continue;
+                while (second<third&&nums[second]+nums[third]>target)--third;
+
+                if (second==third)break;
+                if (nums[second]+nums[third]==target){
+                    List<Integer> list = new ArrayList<Integer>();
+                    list.add(nums[first]);
+                    list.add(nums[second]);
+                    list.add(nums[third]);
+                    ans.add(list);
+                }
+            }
+        }
+        return ans;
     }
     public static void main(String[] args) {
         int[] testData = {-1,0,1,2,-1,-4};
